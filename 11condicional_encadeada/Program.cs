857177e2841b747206e5d0546d099404f073a031
ozaeltton_matheus <hardlevel26062004@gmail.com>
2025-10-23 11:26:35 -0300

    Criaçãp dp projeto de condicionais encadeadas

---
+++ 11condicional_encadeada/Program.cs
@@ -0,0 +1,20 @@
+﻿// Condicional encadeada
+
+// Criaçãp da variável media
+double media = 8;
+
+// Estrutura condicional encadeada
+if (media >= 6)
+{
+    Console.WriteLine("Você foi aprovado(a)!");
+}
+
+else if (media >= 5)
+{
+    Console.WriteLine("Você está em recuperação!");
+}
+
+else
+{
+    Console.WriteLine("Você foi reprovado(a)!");
+}
