6db25ab7dd9a3df6c5879bccde4d7199b62cc0c9
Ozaeltton Matheus <hardlevel266062004@gmail.com>
2025-10-29 09:37:37 -0300

    Adicionado o projeto 12condicional_operadores_logicos

--- 02variaveis/obj/Debug/net9.0/02variaveis.AssemblyInfo.cs
+++ 02variaveis/obj/Debug/net9.0/02variaveis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02variaveis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390973935b4d6d75475d9b60049431527a5de37f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857177e2841b747206e5d0546d099404f073a031")]
 [assembly: System.Reflection.AssemblyProductAttribute("02variaveis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02variaveis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 12condicional_operadores_logicos/Program.cs
@@ -0,0 +1,16 @@
+﻿// Criação das variáveis
+Console.WriteLine("Qual é o valor da compra?\nDigite o valor da compra: ");
+double ?valor_da_compra = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Qual será o tipo de pagamento?\nDIgite o tipo do pagamento\nDigite \n(1) Para Á vista \n(2) para à praso ");
+double? forma_de_pagamento = Convert.ToDouble(Console.ReadLine());
+
+if (valor_da_compra >= 100 && forma_de_pagamento == 1)
+{
+    Console.WriteLine($"Desconto de 10 %! total a pagar RS{valor_da_compra * 0.9}");
+}
+
+else
+{
+    Console.WriteLine($"O total da compra foi R${valor_da_compra}");
+}
+
