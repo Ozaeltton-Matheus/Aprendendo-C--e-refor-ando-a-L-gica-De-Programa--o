09a52b303b483476b6908db55499de1cc105ac27
Ozaeltton Matheus <hardlevel266062004@gmail.com>
2026-01-07 14:42:02 -0300

    Adicionando projeto 15 Laço de repetição while (enquanto).

---
+++ 15laco_enquanto/Program.cs
@@ -0,0 +1,11 @@
+﻿// Estrutura de repetição enquanto (while)
+
+// Váriavel de indice
+int indice = 1;
+
+//laço de repetição enquanto
+while(indice <= 10)
+{
+    Console.WriteLine(indice);
+    indice++; // É necessário incrementar com o indice++ para que o laço de repetição não fique em um cilo infinito exibindo somente 1
+}
