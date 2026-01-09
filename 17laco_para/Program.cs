132d234f9a2fc358e017d15e19c365aaa65ebaa0
Ozaeltton Matheus <hardlevel266062004@gmail.com>
2026-01-09 14:12:11 -0300

    Criação do projeto 17 Laço para (for).

---
+++ 17laco_para/Program.cs
@@ -0,0 +1,5 @@
+﻿// laço de repetição para
+for (int indice = 1; indice <= 10; indice++) // Primeiro é preciso infornar uma variavel com o valor inicial do laço de repetição e a segunda informação é o número final, ou seja, atéque ponto o laço de repetição vai ser executado
+{
+  Console.WriteLine(indice);  
+}
